7e6dba17e49ab2025a9e41a2e752dfcf0952b7e0
Alessandro Caliaro <alessandro.caliaro@soluzione1.it>
2023-03-03 18:44:33 +0100

    Aggiungere i file di progetto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		BindingContext = new MainViewModel();
+	}
+
+	
+}
+
---
+++ MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.ObjectModel;
+
+namespace MauiApp4
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        ObservableCollection<string> lista;
+
+        public MainViewModel()
+        {
+            Lista = new ObservableCollection<string>();
+        }
+
+        [RelayCommand]
+        private void add()
+        {
+            Lista.Add("AAAAA");
+        }
+
+
+        [RelayCommand]
+        private void remove()
+        {
+            if(Lista.Count > 0)
+            {
+                Lista.RemoveAt(0);
+            }
+        }
+
+
+    }
+}
